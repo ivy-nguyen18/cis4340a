b062adee8c0af5bb7725e511c97da72f0a377ce2
Ivy Nguyen <nguyen.ivy1802@gmail.com>
2024-03-01 23:30:54 -0500

    created R13 with noncompliant code

---
+++ R13_FIO09_J.java
@@ -0,0 +1,12 @@
+// Rule 13. Input Output (FIO)
+
+public class R013_FIO09_J {
+    // FIO09-J. Do not rely on the write() method to output integers outside the range 0 to 255. Given the non-compliant code below:
+    class ConsoleWrite {
+        public static void main(String[] args) {
+          // Any input value > 255 will result in unexpected output
+          System.out.write(Integer.valueOf(args[0]));
+          System.out.flush();
+        }
+      } 
+}
