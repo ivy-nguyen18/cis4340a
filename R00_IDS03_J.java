b8303c96645945eae00ad16b6d5ed2518ba3f6d0
Ivy Nguyen <nguyen.ivy1802@gmail.com>
2024-03-01 22:16:10 -0500

    Revised code to be compliant

---
+++ R00_IDS03_J.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *  Rule 00. Input Validation and Data Sanitization (IDS)
+ *
+ * Revision History:
+ *  //IDS03-J. Do not log unsanitized user input Given the non-compliant code below:
+    if (loginSuccessful){
+        logger.severe("User login succeeded for: " + username);
+    } else {
+        logger.severe("User login failed for: " + username);
+    } 
+ *
+ ******************************************************************************/
+
+public class R00_IDS03_J {
+
+    // Revised with Compliant Code
+    if (loginSuccessful) {
+        logger.severe("User login succeeded for: " + sanitizeUser(username));
+      } else {
+        logger.severe("User login failed for: " + sanitizeUser(username));
+      }
+    
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username))
+            ? username : "unauthorized user";
+    }
+} 
