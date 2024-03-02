6175df58a85cd44e68dad3a0cbd23816a9c4fa3a
Ivy Nguyen <nguyen.ivy1802@gmail.com>
2024-03-01 23:15:06 -0500

    revised with compliant code

---
+++ R05_OBJ05_J.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * Rule 05. Object Orientation (OBJ)
+ *
+ * Revision History:
+    // OBJ05-J. Do not return references to private mutable class members
+    class MutableClass {
+        private Date d;
+       
+        public MutableClass() {
+          d = new Date();
+        }
+       
+        public Date getDate() {
+          return d;
+    }
+******************************************************************************/
+
+public class R05_OBJ05_J {
+    // OBJ05-J. Do not return references to private mutable class members
+    class MutableClass {
+        private Date d;
+       
+        public MutableClass() {
+          d = new Date();
+        }
+       
+        public Date getDate() {
+          return d;
+        }
+      }
+    // Revised with compliant code
+    public Date getDate() {
+        return (Date)d.clone();
+      }
+}
