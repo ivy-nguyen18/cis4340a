ac189a91601a33514d6bbfb1f05f07b40959d1cb
Ivy Nguyen <nguyen.ivy1802@gmail.com>
2024-03-01 22:19:43 -0500

    Created R02 with noncompliant code

---
+++ R02_XP00_J.java
@@ -0,0 +1,12 @@
+//Rule 02. Expressions (EXP)
+
+import java.io.File;
+
+public class R02_XP00_J {
+    //This noncompliant code example attempts to delete a file but fails to check whether the operation has succeeded:
+    public void deleteFile(){
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    someFile.delete();
+    }
+}
