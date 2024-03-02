72d988b7013865b9a16e05436f474b8af6563564
Ivy Nguyen <nguyen.ivy1802@gmail.com>
2024-03-01 22:51:11 -0500

    Created R06 noncompliant code

---
+++ R06_MET01_J.java
@@ -0,0 +1,13 @@
+//Rule 06. Methods (MET)
+public class R06_MET01_J {
+    //MET01-J. Never use assertions to validate method arguments Given the non-compliant code below:
+
+public static int getAbsAdd(int x, int y) {
+    assert x != Integer.MIN_VALUE;
+    assert y != Integer.MIN_VALUE;
+    int absX = Math.abs(x);
+    int absY = Math.abs(y);
+    assert (absX <= Integer.MAX_VALUE - absY);
+    return absX + absY;
+  }
+}
