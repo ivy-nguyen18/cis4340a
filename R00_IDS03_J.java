9123e3573d725af66f5037070badb11d12e133e0
Ivy Nguyen <nguyen.ivy1802@gmail.com>
2024-03-01 21:58:38 -0500

    Added problematic code to R00

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,4 +1,9 @@
 //Rule 00. Input Validation and Data Sanitization (IDS)
 public class R00_IDS03_J {
-    
+    //IDS03-J. Do not log unsanitized user input Given the non-compliant code below:
+    if (loginSuccessful){
+        logger.severe("User login succeeded for: " + username);
+    } else {
+        logger.severe("User login failed for: " + username);
+    }
 }
